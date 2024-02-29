d0fc4516009d8a5b68fc70caddf2a776a398939e
hejo47 <hejo_47@naver.com>
2024-02-29 16:52:37 +0900

    feat: select 1차 작업

--- js/common.js
+++ js/common.js
@@ -14,3 +14,49 @@ function preventDefault() {
     },false)
   })
 }
+
+// Select
+$(".select").on("click", function(){
+  $(this).addClass("active");
+  $(this).children("ul").fadeIn("fast").scrollTop();
+
+  if($(this).hasClass("active")) {
+    $(".select").not(this).removeClass("active");
+  }
+
+  let headerOffset = $(this).offset().top;
+  $(".header").animate({
+    scrollTop : headerOffset - 20
+  }, 400);
+});
+
+$(".select").children("ul").on("click", ".select__item", function(e){
+  let selectValue = $(this).data("value"); // 보이는 ul의 값
+  let selectText = $(this).text();
+  let selectID = $(this).closest(".select").data("id");
+
+  let selectHref = $(this).data("href");
+
+  e.stopPropagation();
+
+  $(".select__item").removeClass('selected');
+  $(this).addClass('selected');
+
+  $(".select_wrap select option").each(function(){
+    if($(this).val() == selectValue) {
+      $(this).prop("selected", true);
+      $(this).val(selectValue).change();
+    }
+  });
+
+  $(`.select__value#${selectID}`).text(selectText);
+
+  $(".select").removeClass("active");
+
+  // 현재 필요한 기능
+  // 상품 설명 선택했을 때 href 이동
+  location.href = location.origin + selectHref;
+  console.log(location);
+  // selected 옮겨가기
+
+});
